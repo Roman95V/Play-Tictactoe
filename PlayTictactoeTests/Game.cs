8876389f74cf594131abce4deb1f90a83eb55a8d
voloshynromanmukol@gmail.com <voloshynromanmukol@gmail.com>
2021-05-06 18:05:23 +0300

    tests

--- PlayTictactoeTests/Game.cs
+++ PlayTictactoeTests/Game.cs
@@ -10,7 +10,7 @@ namespace PlayTictactoeTests
     public class Game
     {
         private readonly IWebDriver _webDriver;
-
+        private By _twoPlayers = By.CssSelector("path.p1");
         private By _cellTopLeft = By.CssSelector("div.square:nth-child(1)");
         private By _cellTopRight = By.CssSelector("div.square:nth-child(3)");
         private By _cellTop = By.CssSelector("div.square:nth-child(2)");
@@ -20,8 +20,7 @@ namespace PlayTictactoeTests
         private By _cellBottomLeft = By.CssSelector("div.square:nth-child(7)");
         private By _cellBottomRight = By.CssSelector("div.square:nth-child(9)");
         private By _cellBottom = By.CssSelector("div.square:nth-child(8)");
-        private By _accountBlockMassage = By.CssSelector("div.FormErrorText__error---nzyq:nth-child(1) > div:nth-child(1)");
-        private By _x = By.CssSelector("div.x");
+        private  By _x = By.CssSelector("div.x");
 
         public Game(IWebDriver webDriver)
         {
@@ -37,6 +36,21 @@ namespace PlayTictactoeTests
         public void ClickCellTopLeft()
         {
             _webDriver.FindElement(_cellTopLeft).Click();
+        }
+
+        public void ClickCelliTheMiddleOnTheLeftt()
+        {
+            _webDriver.FindElement(_celliTheMiddleOnTheLeft).Click();
+        }
+
+        public void ClickCellBottomLeftt()
+        {
+            _webDriver.FindElement(_cellBottomLeft).Click();
+        }
+
+        public void ClickPlayTwoPlayers()
+        {
+            _webDriver.FindElement(_twoPlayers).Click();
         }
     }
 }
--- PlayTictactoeTests/GameTests.cs
+++ PlayTictactoeTests/GameTests.cs
@@ -1,13 +1,65 @@
-﻿using System;
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using WebDriverManager;
+using WebDriverManager.DriverConfigs.Impl;
 
 namespace PlayTictactoeTests
 {
-    public class GameTests
+    public class GameTest
     {
+        private IWebDriver _webDriver;
+        [SetUp]
+        public void Setup()
+        {
+            new DriverManager().SetUpDriver(new FirefoxConfig());
+            _webDriver = new FirefoxDriver();
+            _webDriver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(7);
+            _webDriver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(60);
 
+        }
+        [TearDown]
+        public void TearDown()
+        {
+            _webDriver.Quit();
+        }
+        [Test]
+        public void CheckTheXChaActerDisplay()
+        {
+            var game = new Game(_webDriver);
+            game.OpenPage();
+            game.ClickCellTopLeft();
+
+          //  game.ClickCellTopLeft();
+          //  game.ClickCelliTheMiddleOnTheLeftt();
+          //  game.ClickCellTopLeft();
+          //
+            var actualresult = _webDriver.FindElement(By.CssSelector("div.x"));
+            var result = _webDriver.FindElement(By.CssSelector("div.square:nth-child(1)"));
+
+            Assert.AreNotEqual(result, actualresult);
+
+        }
+
+        [Test]
+        public void Check()
+        {
+            var game = new Game(_webDriver);
+            game.OpenPage();
+            
+            game.ClickPlayTwoPlayers();
+            Thread.Sleep(3000);
+            var actualresult = _webDriver.FindElement(By.CssSelector("p.p1"));
+            var result = _webDriver.FindElement(By.CssSelector("p.p2"));
+           
+            Assert.AreNotEqual(result, actualresult);
+
+        }
     }
 }
